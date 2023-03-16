ee4adc88c9f84e6abc9a4587a2e34fa92c80acdc
Italo-Alan <italoalan123@gmail.com>
2023-03-16 11:32:26 -0300

    Consumo da API feito

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeadeValue: '0de1d1e992msh0150ff931f8e834p1e02c9jsn9f658813f8c8',
+
+}
