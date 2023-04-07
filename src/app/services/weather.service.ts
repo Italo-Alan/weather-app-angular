7e182c2f42273a6f748a45ce64eaa55b8188a5e8
Italo-Alan <italoalan123@gmail.com>
2023-04-06 23:43:32 -0300

    feat: Fazendo funcionar o deploy

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -10,9 +10,16 @@ import { WeatherData } from '../models/weather.model';
   providedIn: 'root'
 })
 export class WeatherService {
+  API_BASE_URL: String = 'http://api.openweathermap.org/data/2.5/weather?q='
+  API_KEY: String = '1419820775eaf67d81bdaeb18adafa16'
+  UNITS: String ='metric'
+  LANG: String ='pt_br'
+  API_BASE_URL_3_0: String = 'https://api.openweathermap.org/data/3.0/onecall?'
+
+
   constructor(private http: HttpClient) { }
   getWeatherData(city: String){
-    return this.http.get<WeatherData>(`${environment.API_BASE_URL}${city}&appid=${environment.API_KEY}&units=${environment.UNITS}&lang=${environment.LANG}`);
+    return this.http.get<WeatherData>(`${this.API_BASE_URL}${city}&appid=${this.API_KEY}&units=${this.UNITS}&lang=${this.LANG}`);
   }
 
 }
